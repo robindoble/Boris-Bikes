023cc10587da70b159a4962f35e0646311d84cec
Robin <robindoble@gmai.com>
2014-04-14 17:12:59 +0100

    docking station now knows when its capacity is reached and raises an error

---
+++ lib/docking_station.rb
@@ -0,0 +1,29 @@
+class DockingStation
+
+DEFAULT_CAPACITY=10
+
+def initialize(options={})
+	@capacity = options.fetch(:capacity,DEFAULT_CAPACITY)
+	@bikes = []
+end
+
+def bike_count
+	@bikes.count
+end
+
+def dock(bike)
+#if capacity is reached, raise an exception
+raise "Station is full" if full?
+@bikes << bike
+end
+
+def release(bike)
+	@bikes.delete(bike)
+end
+
+def full?
+	bike_count==@capacity
+end
+
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../lib/docking_station'
+require_relative '../lib/bike'
+
+describe DockingStation do
+	
+let(:bike) { Bike.new }
+let(:station) { DockingStation.new(:capacity => 20) }
+
+def fill_station(station)
+	20.times {station.dock(Bike.new)}
+end
+
+it "should accept a bike" do
+	expect(station.bike_count).to eq(0)
+	station.dock(bike)
+	expect(station.bike_count).to eq(1)
+end
+
+it "should be able to release a bike" do
+	station.dock(bike)
+	station.release(bike)
+	expect(station.bike_count).to eq(0)
+end
+
+it "should know when its full" do
+	expect(station).not_to be_full
+	fill_station(station)
+	# 20.times {station.dock(Bike.new)}
+	expect(station).to be_full
+end
+
+it "should not accept a bike if full" do
+	fill_station(station)
+	# 20.times {station.dock(Bike.new)}
+	expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
+end
+
+end
