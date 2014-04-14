9ba3137279e7585dafda15e10f7f879d05b3ba38
Robin <robindoble@gmai.com>
2014-04-14 15:27:43 +0100

    refactored Bike class methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,19 +2,21 @@ require_relative "../lib/bike"
 
 describe Bike do 
 	
+let(:bike) {Bike.new}
+
 it "knows that a new bike is not broken" do
-bike = Bike.new
+# bike = Bike.new
 expect(bike).not_to be_broken
 end
 
 it "knows that a broken bike is broken" do
-bike = Bike.new
+# bike = Bike.new
 bike.break
 expect(bike).to be_broken
 end
 
 it "knows how to fix a bike as well" do
-bike = Bike.new
+# bike = Bike.new
 bike.break
 bike.fix
 expect(bike).not_to be_broken
