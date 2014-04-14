59fb870d231efa7157ac8589aa9c9adf7750b47d
Robin <robindoble@gmai.com>
2014-04-14 13:32:58 +0100

    created a lib and spec directory structure and the first test for the class Bike - a new bike must NOT be broken

---
+++ spec/bike_spec.rb
@@ -0,0 +1,14 @@
+require 'bike'
+
+describe Bike do 
+	
+# it "knows that a new bike is not broken" do
+# expect(Bike.new).to be_false
+# end
+
+it "knows that a new bike is not broken" do
+bike = Bike.new
+expect(bike).not_to be_broken
+end
+
+end
